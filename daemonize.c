c6a8d43a64db329139f29572829138c12a83f96a
Rafał Popławski <rav96pl@gmail.com>
2017-03-31 21:55:19 +0200

    Zapis do logu systemowego lub innego pliku w zależności od zdefiniowanej stałej

--- daemonize.c
+++ daemonize.c
@@ -1,9 +1,3 @@
-/*
- * daemonize.c
- * This example daemonizes a process, writes a few log messages,
- * sleeps 20 seconds and terminates afterwards.
- */
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/wait.h>
@@ -17,8 +11,38 @@
 
 #define EXIT_SUCCESS 0
 #define EXIT_FAILURE 1
+#define USE_SYSLOG 1
 
+#if !USE_SYSLOG
 int logfd;
+#endif
+
+void openLogFile()
+{
+    #if USE_SYSLOG
+    openlog("firstdaemon", LOG_PID, LOG_DAEMON);
+    #else
+    logfd = open("filesyncdaemon.log", O_WRONLY | O_CREAT | O_APPEND, 0644);
+    #endif
+}
+
+void closeLogFile()
+{
+    #if USE_SYSLOG
+    closelog();
+    #else
+    close(logfd);
+    #endif
+}
+
+void writeToLog(char *str)
+{
+    #if USE_SYSLOG
+    syslog(LOG_NOTICE, str);
+    #else
+    write(logfd, str, strlen(str));
+    #endif
+}
 
 static void skeleton_daemon()
 {
@@ -34,7 +58,7 @@ static void skeleton_daemon()
     /* Success: Let the parent terminate */
     if (pid > 0)
     {
-	//wait(NULL);
+	    //wait(NULL);
         exit(EXIT_SUCCESS);
     }
 
@@ -57,7 +81,7 @@ static void skeleton_daemon()
     /* Success: Let the parent terminate */
     if (pid > 0)
     {
-	//wait(NULL);
+	    //wait(NULL);
         exit(EXIT_SUCCESS);
     }
 
@@ -66,7 +90,9 @@ static void skeleton_daemon()
 
     /* Change the working directory to the root directory */
     /* or another appropriated directory */
-    //chdir("/");
+    #if USE_SYSLOG
+    chdir("/");
+    #endif
 
     /* Close all open file descriptors */
     int x;
@@ -76,23 +102,17 @@ static void skeleton_daemon()
     }
 
     /* Open the log file */
-    //openlog ("firstdaemon", LOG_PID, LOG_DAEMON);
-}
-
-void openLogFile()
-{
-    logfd = open("mydaemon_log", O_WRONLY | O_APPEND, 0644);
-}
-
-void writeToLog(char *str)
-{
-    write(logfd, str, strlen(str));
-}
-
-int main()
-{    
-    skeleton_daemon();
     openLogFile();
+}
+
+int main(int argc, char* argv[])
+{   
+    if (argc < 3 || argc > 4)
+    {
+        printf("Usage: %s source_path destination_path [-R]\n", argv[0]);
+        return 0;
+    }
+    skeleton_daemon();
 
     writeToLog("First daemon started.\n");
     char i = '1';
@@ -102,19 +122,16 @@ int main()
     while (1)
     {
         //TODO: Insert daemon code here.
-        //syslog (LOG_NOTICE, "First daemon started.");
-        
+
         sleep(5);
-	str[4] = i;
-	writeToLog(str);
-	printf(str);
+        str[4] = i;
+        writeToLog(str);
         i++;
         if (i > '5') break;
     }
 
-    /*syslog (LOG_NOTICE, "First daemon terminated.");
-    closelog();*/
-    close(logfd);
+    writeToLog("First daemon terminated.\n");
+    closelog();
 
     return EXIT_SUCCESS;
 }
